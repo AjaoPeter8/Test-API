437205f2e755063b385e143514a34b5d85d4a688
ajaopeter8 <oluwafemipeter02@gmail.com>
2025-10-18 17:11:15 +0100

    created a test api to fetch user's details and cats facts

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const now = new Date();
+const timestamp = now.toISOString();
+
+let fact = "";
+
+let user = {
+    email: "oluwafemipeter02@gmail.com",
+    name: "Ajao Peter Oluwafemi",
+    stack: "Node.js/Express"
+}
+    const status = "Success";
+
+
+
+app.get("/me", async (req, res) => {
+
+    try {
+    const response = await axios.get("https://catfact.ninja/fact", { timeout: 5000 });
+    fact = response.data.fact;
+
+    console.log(fact);
+}
+catch (error) {
+    fact = `Failed to fetch`;
+    console.log(error);
+}
+
+console.log(timestamp);
+    console.log(user);
+    res.json({ status, user, timestamp, fact});
+})
+
+app.listen(port, () => {
+  console.log(`Running on http://localhost:${port}`);
+});
